3fd8ce04ecc2db9a532d155795fa2d3e423c606a
Anton Holmgren <me@antonholmgren.com>
2024-03-19 10:53:43 +0100

    Remove null-ls
    No longer maintained

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -164,13 +164,6 @@ function M.setup()
       },
     }
     use {
-      "jose-elias-alvarez/null-ls.nvim",
-      config = function()
-        require("config.null-ls").setup()
-      end,
-      requires = { "nvim-lua/plenary.nvim" },
-    }
-    use {
       "j-hui/fidget.nvim",
       tag = "legacy",
       config = function()
