611bfe97a8222e2bb7257fbd52ccc29fae5fc811
Anton Holmgren <me@antonholmgren.com>
2024-08-24 23:24:16 +0200

    Use full interface to keymap

--- nvim/after/plugin/keymaps.lua
+++ nvim/after/plugin/keymaps.lua
@@ -1,22 +1,20 @@
-local keymap = vim.api.nvim_set_keymap
 local default_opts = { noremap = true, silent = true }
-local expr_opts = { noremap = true, expr = true, silent = true }
 
-keymap("v", "<C-c>", ":y +<CR>", default_opts)
+vim.keymap.set("v", "<C-c>", ":y +<CR>", default_opts)
 
-keymap(
+vim.keymap.set(
   "n",
   "<C-e>",
   "<cmd>lua require('telescope.builtin').find_files({hidden=true})<cr>",
   default_opts
 )
-keymap(
+vim.keymap.set(
   "n",
   "<C-p>",
   "<cmd>lua require('telescope').extensions.project.project()<cr>",
   default_opts
 )
-keymap(
+vim.keymap.set(
   "n",
   "<C-b>",
   "<cmd>lua require('telescope.builtin').buffers()<cr>",
@@ -26,4 +24,5 @@ vim.keymap.set('i', '<C-a>', 'copilot#Accept("<CR>")', {
   expr = true,
   replace_keycodes = false
 })
+
 vim.g.copilot_no_tab_map = true
