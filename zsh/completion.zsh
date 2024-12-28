8a51f6083c351245ad63e3744b157ad46360141c
Anton Holmgren <me@antonholmgren.com>
2024-12-28 11:05:59 +0100

    Remove obsolete completion

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -31,21 +31,3 @@ function _fzf_complete_gh() {
 function _fzf_complete_gh_post() {
   awk '{gsub("#", ""); print $1}'
 }
-
-function _fzf_complete_val() {
-  ARGS="$@"
-  local names
-  if [[ $ARGS == "val"*"--training-job"* ]]; then
-    names="$(val list training-jobs)"
-    _fzf_complete "--reverse --multi" "$@" < <(
-      echo "${names}"
-    )
-  elif [[ $ARGS == "val"*"--model"* ]]; then
-    names="$(val list models)"
-    _fzf_complete "--reverse --multi" "$@" < <(
-      echo "${names}"
-    )
-  else
-    eval "zle ${fzf_default_completion:-expand-or-complete}"
-  fi
-}
