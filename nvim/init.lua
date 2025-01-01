5fe01242240795d7e5f5c6a395fa4af5b6966430
Anton Holmgren <me@antonholmgren.com>
2025-01-01 20:51:48 +0100

    Remove linenumbers in term buffers

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,1 +1,10 @@
 require("config.lazy")
+
+vim.api.nvim_create_autocmd("TermOpen", {
+  group = vim.api.nvim_create_augroup("custom-term-open", { clear = true }),
+  callback = function()
+    vim.opt.number = false
+    vim.opt.relativenumber = false
+  end,
+})
+
