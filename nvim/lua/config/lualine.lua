51053fb0cf33e7b103d5fc68bcca68a313ca679e
Anton Holmgren <anton.holmgren@hemnet.se>
2026-01-14 23:04:41 +0100

    Remove parrot.nvim

--- nvim/lua/config/lualine.lua
+++ nvim/lua/config/lualine.lua
@@ -1,16 +1,5 @@
 local M = {}
 
-local function parrot_status()
-  local status_info = require("parrot.config").get_status_info()
-  local status = ""
-  if status_info.is_chat then
-    status = status_info.prov.chat.name
-  else
-    status = status_info.prov.command.name
-  end
-  return string.format("%s(%s)", status, status_info.model)
-end
-
 function M.setup()
   require("lualine").setup({
     options = {
@@ -38,7 +27,6 @@ function M.setup()
           },
         },
       },
-      lualine_x = { parrot_status, "encoding", "fileformat", "filetype" },
       lualine_y = { "progress" },
       lualine_z = { "location" },
     },
--- nvim/lua/config/plugins/ai.lua
+++ nvim/lua/config/plugins/ai.lua
@@ -1,12 +1,3 @@
 return {
   { "github/copilot.vim" },
-  { "frankroeder/parrot.nvim",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "ibhagwan/fzf-lua",
-    },
-    config = function()
-      require("config.parrot").setup()
-    end,
-  },
 }
