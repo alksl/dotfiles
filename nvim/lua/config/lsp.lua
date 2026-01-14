39f74b368d3e76922da87ca7b09bd5027bb66f07
Anton Holmgren <anton.holmgren@hemnet.se>
2026-01-14 23:02:50 +0100

    Use latest lspconfig style

--- nvim/lua/config/lsp.lua
+++ nvim/lua/config/lsp.lua
@@ -3,30 +3,32 @@ local M = {}
 function M.setup()
   require("mason").setup()
   require("mason-lspconfig").setup {
+    automatic_enable = {
+      "tofu_ls",
+    },
     ensure_installed = {
       "lua_ls",
       "rust_analyzer",
       "ruby_lsp",
       "jsonls",
       "yamlls",
-      "terraformls",
-      "tflint",
       "bashls",
       "pyright",
       "ruff",
       "rubocop",
-      "ts_ls",
+      "denols",
+      "tofu_ls",
+      "gopls",
     }
   }
 
-  local lspconfig = require("lspconfig")
   local navic = require("nvim-navic")
   local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
   local on_attach = function(client, bufnr)
-      if client.server_capabilities.documentSymbolProvider then
-          navic.attach(client, bufnr)
-      end
+    if client.server_capabilities.documentSymbolProvider then
+      navic.attach(client, bufnr)
+    end
   end
 
   local opts = {
@@ -34,31 +36,108 @@ function M.setup()
     capabilities = capabilities,
   }
 
-  lspconfig.ts_ls.setup(opts)
-  lspconfig.ruby_lsp.setup({
+  -- Add these helper functions after line 26
+  local function find_project_root(fname, markers)
+    local path = vim.fn.fnamemodify(fname, ':p:h')
+    while path ~= '/' do
+      for _, marker in ipairs(markers) do
+        if vim.loop.fs_stat(path .. '/' .. marker)
+        then
+          return path
+        end
+      end
+      path = vim.fn.fnamemodify(path, ':h')
+    end
+    return nil
+  end
+
+  local function has_deno_config(fname)
+    return find_project_root(fname, {'deno.json',
+      'deno.jsonc'}) ~= nil
+  end
+
+  local function has_package_json(fname)
+    return find_project_root(fname, {'package.json'})
+      ~= nil
+  end
+
+  vim.lsp.config('gopls', {
+    on_attach = on_attach,
+    capabilities = capabilities,
+    settings = {
+      gopls = {
+        staticcheck = true,
+      }
+    }
+  })
+  vim.lsp.enable('gopls')
+  vim.lsp.config('ts_ls', {
     capabilities = capabilities,
     on_attach = on_attach,
-    cmd = {"bundle", "exec", "ruby-lsp"},
+    root_dir = function(bufnr, on_dir)
+      local fname = vim.api.nvim_buf_get_name(bufnr)
+
+      if has_package_json(fname) and not has_deno_config(fname) then
+        local root = find_project_root(fname, {'package.json'})
+        if root then
+          on_dir(root)
+        end
+      end
+      -- Don't call on_dir() to skip activation
+    end
   })
-  lspconfig.rubocop.setup({
+  vim.lsp.enable('ts_ls')
+  vim.lsp.config('denols', {
     capabilities = capabilities,
-    cmd = { "bundle", "exec", "rubocop", "--lsp" },
+    on_attach = on_attach,
+    root_dir = function(bufnr, on_dir)
+      local fname = vim.api.nvim_buf_get_name(bufnr)
+
+      -- Start denols if there's a deno config
+      local deno_root = find_project_root(fname, {'deno.json', 'deno.jsonc'})
+      if deno_root then
+        on_dir(deno_root)
+        return
+      end
+
+      -- For single files without package.json, use file directory
+      if not has_package_json(fname) then
+        on_dir(vim.fn.fnamemodify(fname, ':p:h'))
+        return
+      end
+      -- Don't call on_dir() to skip activation
+    end,
   })
-  lspconfig.terraformls.setup(opts)
-  lspconfig.tflint.setup(opts)
-  lspconfig.lua_ls.setup({
+  vim.lsp.enable('denols')
+  vim.lsp.config('ruby_lsp', {
+    capabilities = capabilities,
+    on_attach = on_attach,
+    cmd = { "bundle", "exec", "ruby-lsp" },
+  })
+  vim.lsp.enable('ruby_lsp')
+  -- vim.lsp.config('rubocop', {
+  --   capabilities = capabilities,
+  --   cmd = { "bundle", "exec", "rubocop", "--lsp" },
+  -- -- })
+  -- vim.lsp.enable('ruby_lsp')
+  -- vim.lsp.config('terraformls', opts)
+  -- viml.lsp.enable('terraformls')
+  vim.lsp.config("tofu_ls", opts)
+  -- vim.lsp.config('tflint', opts)
+  -- vim.lsp.enable('tflint')
+  vim.lsp.config('lua_ls',{
     capabilities = capabilities,
     on_attach = on_attach,
     settings = {
       Lua = {
         runtime = {
-          -- Tell the language server which version of Lua you're 
+          -- Tell the language server which version of Lua you're
           -- using (most likely LuaJIT in the case of Neovim)
           version = 'LuaJIT',
         },
         diagnostics = {
           -- Get the language server to recognize the `vim` global
-          globals = {'vim'},
+          globals = { 'vim' },
         },
         workspace = {
           -- Make the server aware of Neovim runtime files
@@ -71,8 +150,9 @@ function M.setup()
       },
     },
   })
+  vim.lsp.enable('lua_ls')
 
-  lspconfig.jsonls.setup({
+  vim.lsp.config('jsonls', {
     capabilities = capabilities,
     on_attach = on_attach,
     settings = {
@@ -82,7 +162,8 @@ function M.setup()
       },
     },
   })
-  lspconfig.yamlls.setup({
+  vim.lsp.enable('jsonls')
+  vim.lsp.config('yamlls', {
     capabilities = capabilities,
     on_attach = on_attach,
     settings = {
@@ -98,9 +179,12 @@ function M.setup()
       },
     },
   })
-  lspconfig.bashls.setup(opts)
-  lspconfig.pyright.setup(opts)
-  lspconfig.ruff.setup({
+  vim.lsp.enable('yamlls')
+  vim.lsp.config('bashls', opts)
+  vim.lsp.enable('bashls')
+  vim.lsp.config('pyright', opts)
+  vim.lsp.enable('pyright')
+  vim.lsp.config('ruff', {
     capabilities = capabilities,
     on_attach = on_attach,
     init_options = {
@@ -109,13 +193,14 @@ function M.setup()
       }
     }
   })
-  lspconfig.rust_analyzer.setup({
+  vim.lsp.enable('ruff')
+  vim.lsp.config('rust_analyzer', {
     capabilities = capabilities,
     on_attach = function(client, bufnr)
       if client.server_capabilities.documentSymbolProvider then
         navic.attach(client, bufnr)
       end
-      vim.lsp.inlay_hint.enable(true, {bufnr = bufnr})
+      vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
     end,
     settings = {
       ["rust_analyzer"] = {
@@ -138,9 +223,10 @@ function M.setup()
       },
     }
   })
+  vim.lsp.enable('rust_analyzer')
 
-  vim.api.nvim_create_autocmd({"BufWritePre"}, {
-    pattern = {"*.py", "*.tf", "*.tfvars", "*.rb"},
+  vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+    pattern = { "*.py", "*.tf", "*.tfvars", "*.rb", "*.ts" },
     callback = function()
       vim.lsp.buf.format()
     end,
@@ -184,7 +270,7 @@ function M.setup()
         end, key_opts)
         vim.keymap.set('n', '<Leader>la', vim.lsp.buf.code_action, key_opts)
       end,
-  })
+    })
 end
 
 return M
