5da7ce9b0a58b1bd63235517e7b8c9824e8f94a6
Anton Holmgren <anton.holmgren@hemnet.se>
2026-02-04 20:51:31 +0100

    Get code companion

--- nvim/lua/config/plugins/ai.lua
+++ nvim/lua/config/plugins/ai.lua
@@ -1,3 +1,24 @@
 return {
   { "github/copilot.vim" },
+  {
+    "olimorris/codecompanion.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    opts = {
+      -- NOTE: The log_level is in `opts.opts`
+      opts = {
+        log_level = "DEBUG", -- or "TRACE"
+      },
+      strategies = {
+        chat = {
+          adapter = {
+            name = "opencode",
+            model = "claude-sonnet-4.4",
+          },
+        },
+      },
+    },
+  },
 }
